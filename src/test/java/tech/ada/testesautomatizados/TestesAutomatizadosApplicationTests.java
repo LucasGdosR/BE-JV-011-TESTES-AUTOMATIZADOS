3b1208da95108460d4cf918653e157a702bbd603
LucasGdosR <109366863+LucasGdosR@users.noreply.github.com>
2023-05-09 16:47:18 -0300

    Initial commit

---
+++ src/test/java/tech/ada/testesautomatizados/TestesAutomatizadosApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.ada.testesautomatizados;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestesAutomatizadosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
