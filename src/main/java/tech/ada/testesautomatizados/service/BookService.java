8cf9e2cc0164c0b4297fd76a9ac73a2728da221c
LucasGdosR <109366863+LucasGdosR@users.noreply.github.com>
2023-05-09 17:40:55 -0300

    feat: service and repository

--- src/main/java/tech/ada/testesautomatizados/service/BookService.java
+++ src/main/java/tech/ada/testesautomatizados/service/BookService.java
@@ -1,28 +1,44 @@
 package tech.ada.testesautomatizados.service;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 import tech.ada.testesautomatizados.model.Book;
+import tech.ada.testesautomatizados.repository.BookRepository;
 
 import java.util.List;
 
 @Service
 public class BookService {
+    private final BookRepository repository;
+
+    public BookService(BookRepository repository) {
+        this.repository = repository;
+    }
+
     public List<Book> findAll() {
-        return null;
+        return repository.findAll();
     }
 
     public Book findById(String id) {
-        return null;
+        return repository.findById(id).orElseThrow(() -> new ResponseStatusException(
+                HttpStatus.NOT_FOUND, "Invalid ISBN."));
     }
 
     public Book save(Book book) {
-        return null;
+        repository.findById(book.getIsbn()).ifPresent(e -> {throw new ResponseStatusException(
+                HttpStatus.CONFLICT, "ISBN already exists.");});
+
+        return repository.save(book);
     }
 
-    public Book editById(String id, Book book) {
-        return null;
+    public Book editById(String id, Book updatedBook) throws ResponseStatusException {
+        findById(id);
+        return repository.save(updatedBook);
     }
 
-    public void deleteById(String id) {
+    public void deleteById(String id) throws ResponseStatusException {
+        findById(id);
+        repository.deleteById(id);
     }
 }
