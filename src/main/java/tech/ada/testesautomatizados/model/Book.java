f056319b3009d6af4ecd4196eb969c9f56823ebe
LucasGdosR <109366863+LucasGdosR@users.noreply.github.com>
2023-05-09 17:25:18 -0300

    feat: attributes and validation

--- src/main/java/tech/ada/testesautomatizados/model/Book.java
+++ src/main/java/tech/ada/testesautomatizados/model/Book.java
@@ -1,8 +1,33 @@
 package tech.ada.testesautomatizados.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.*;
 import lombok.Getter;
+import lombok.Setter;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
 @Getter
+@Setter
 public class Book {
+    @Id
+    @NotBlank
     private String isbn;
+    @NotBlank
+    private String title;
+    @NotBlank
+    @Size(max = 500)
+    private String resumo;
+    private String summary;
+    @NotNull
+    @Min(20)
+    private BigDecimal price;
+    @NotNull
+    @Min(100)
+    private Integer numberOfPages;
+    @Future
+    private LocalDate publishingDate;
 }
