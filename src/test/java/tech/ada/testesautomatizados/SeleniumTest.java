b9a582fb03fac8cde63a4236d5286f53c56507fc
Luiz <felipeluizsantos.k@gmail.com>
2023-05-19 13:54:21 -0300

    Second integration test done, and finished POST tests.

--- src/test/java/tech/ada/testesautomatizados/SeleniumTest.java
+++ src/test/java/tech/ada/testesautomatizados/SeleniumTest.java
@@ -1,12 +1,11 @@
 package tech.ada.testesautomatizados;
 
-
-
 import org.junit.jupiter.api.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.time.Duration;
 
@@ -15,11 +14,13 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class SeleniumTest {
 
     private WebDriver driver;
-
+    String path = "C:\\Users\\luizs\\IdeaProjects\\BE-JV-011-TESTES-AUTOMATIZADOS\\src\\main\\resources\\index.html";
     @BeforeEach
     public void setUp() {
-        System.setProperty("webdriver.chrome.driver","/home/luiz/Documentos/chromedriver");
-        driver = new ChromeDriver();
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--remote-allow-origins=*");
+        System.setProperty("webdriver.chrome.driver","F:\\Luizf\\Downloads\\chromedriver_win32\\chromedriver.exe");
+        driver = new ChromeDriver(chromeOptions);
     }
 
     @AfterEach
@@ -28,15 +29,15 @@ public class SeleniumTest {
     }
     @Test
     public void testListAllBooks() {
-        driver.get("file:///home/luiz/Documentos/FrontEnd/index.html");
-        WebElement table = driver.findElement(By.id("Lista de Livros"));
+        driver.get(path);
+        WebElement table = driver.findElement(By.id("booktable"));
         // Assert the table contains the expected number of rows and columns
-        assertEquals(4, table.findElements(By.tagName("tr")).size());
+        assertEquals(1, table.findElements(By.tagName("tr")).size());
     }
 
     @Test
     public void testFindBookById() {
-        driver.get("file:///home/luiz/Documentos/FrontEnd/index.html");
+        driver.get(path);
         WebElement bookIdInput = driver.findElement(By.id("bookId"));
         WebElement findByIdBtn = driver.findElement(By.id("findByIdBtn"));
         WebElement foundBook = driver.findElement(By.id("foundBook"));
@@ -44,13 +45,13 @@ public class SeleniumTest {
         bookIdInput.sendKeys("123");
         findByIdBtn.click();
 
-        // Assert the found book information is displayed correctly
+
         assertEquals("ID: 123, Title: Book Title", foundBook.getText());
     }
 
     @Test
     public void testSaveBook() {
-        driver.get("file:///home/luiz/Documentos/FrontEnd/index.html");
+        driver.get(path);
         WebElement isbnInput = driver.findElement(By.id("isbn"));
         WebElement titleInput = driver.findElement(By.id("title"));
         WebElement saveBtn = driver.findElement(By.id("saveBtn"));
@@ -62,13 +63,13 @@ public class SeleniumTest {
 
         saveBtn.click();
 
-        // Assert the book is saved successfully
+
         assertEquals("Book saved. Location: /api/books/1", savedBook.getText());
     }
 
     @Test
     public void testEditBook() {
-        driver.get("file:///home/luiz/Documentos/FrontEnd/index.html");
+        driver.get(path);
         WebElement editBookIdInput = driver.findElement(By.id("editBookId"));
         WebElement editIsbnInput = driver.findElement(By.id("editIsbn"));
         WebElement editTitleInput = driver.findElement(By.id("editTitle"));
@@ -78,17 +79,17 @@ public class SeleniumTest {
         editBookIdInput.sendKeys("123");
         editIsbnInput.sendKeys("9876543210");
         editTitleInput.sendKeys("Updated Book");
-        // Set other input values if needed
+        // colocar o resto dos valores
 
         editBtn.click();
 
-        // Assert the book is edited successfully
+
         assertEquals("Book edited: ID: 123, Title: Updated Book", editedBook.getText());
     }
 
     @Test
     public void testDeleteBook() {
-        driver.get("file:///home/luiz/Documentos/FrontEnd/index.html");
+        driver.get(path);
         WebElement deleteBookIdInput = driver.findElement(By.id("Excluir um Livro"));
         WebElement deleteBtn = driver.findElement(By.id("Excluir"));
         WebElement deletedBook = driver.findElement(By.id("deletedBook"));
@@ -97,7 +98,7 @@ public class SeleniumTest {
 
         deleteBtn.click();
 
-        // Assert the book is deleted successfully
+
         assertEquals("Livro Excluído: ID: 123", deletedBook.getText());
     }
 
